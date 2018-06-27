4391e35d30a2cb0918c76cb3c881071b955b9d13
Bob Gregory <bob@made.com>
2018-06-27 20:18:16 +0100

    Basic tests ready for defaults

--- src/factorial/template.py
+++ src/factorial/template.py
@@ -1,5 +1,5 @@
-import inspect
 import collections
+import inspect
 import sys
 
 
@@ -10,7 +10,7 @@ def _make_nmtuple(name, types):
     # Now, both __annotations__ and _field_types are used to maintain compatibility.
     nm_tpl.__annotations__ = nm_tpl._field_types = collections.OrderedDict(types)
     try:
-        nm_tpl.__module__ = sys._getframe(2).f_globals.get('__name__', '__main__')
+        nm_tpl.__module__ = sys._getframe(2).f_globals.get("__name__", "__main__")
     except (AttributeError, ValueError):
         pass
 
@@ -18,11 +18,22 @@ def _make_nmtuple(name, types):
 
 
 # attributes prohibited to set in NamedTuple class syntax
-_prohibited = ('__new__', '__init__', '__slots__', '__getnewargs__',
-               '_fields', '_field_defaults', '_field_types',
-               '_make', '_replace', '_asdict', '_source')
+_prohibited = (
+    "__new__",
+    "__init__",
+    "__slots__",
+    "__getnewargs__",
+    "_fields",
+    "_field_defaults",
+    "_field_types",
+    "_make",
+    "_replace",
+    "_asdict",
+    "_source",
+)
 
-_special = ('__module__', '__name__', '__qualname__', '__annotations__')
+_special = ("__module__", "__name__", "__qualname__", "__annotations__")
+
 
 def parse_bool(name, prefix, env):
 
@@ -32,16 +43,16 @@ def parse_bool(name, prefix, env):
     if val is None or val.isspace():
         return False
 
-    if val == '0':
+    if val == "0":
         return False
 
-    if val == '-1':
+    if val == "-1":
         return False
 
-    if val == '1':
+    if val == "1":
         return True
 
-    if val.upper() == 'FALSE':
+    if val.upper() == "FALSE":
         return False
 
     return True
@@ -49,72 +60,85 @@ def parse_bool(name, prefix, env):
 
 def parse_int(name, prefix, env):
     var = (prefix + name).upper()
-    val = env.get(var)
+    if var not in env:
+        return
+    val = env[var]
+
     return int(val)
 
 
 def parse_str(name, prefix, env):
     var = (prefix + name).upper()
-    return env.get(var)
+    if var not in env:
+        return
+    val = env[var]
+    return env[var]
 
 
 def parse_float(name, prefix, env):
     var = (prefix + name).upper()
-    val = env.get(var)
+    if var not in env:
+        return
+    val = env[var]
     return float(val)
 
 
-_READERS = {
-    int: parse_int,
-    float: parse_float,
-    str: parse_str,
-    bool: parse_bool
-}
+_READERS = {int: parse_int, float: parse_float, str: parse_str, bool: parse_bool}
+
 
 @classmethod
-def build(cls, env=None, prefix=''):
+def build(cls, env=None, prefix=""):
 
-    if prefix and not prefix.endswith('_'):
-        prefix += '_'
+    if prefix and not prefix.endswith("_"):
+        prefix += "_"
 
     args = {}
-    for name, reader in cls.__fields.items():
-        if inspect.isclass(reader):
-            if hasattr(reader, 'build'):
-                args[name] = reader.build(env=env, prefix=(name.upper()+'_'+prefix))
-                continue
-        try:
-            args[name] = reader(name, prefix, env)
-        except ValueError as e:
-            raise ValueError(f"Error parsing '{name}' property of '{cls.__inner_type.__name__}':\n" +
-                    str(e))
 
+    for name, reader in cls.__fields.items():
+        val = None
+        if inspect.isclass(reader):
+            if hasattr(reader, "build"):
+                val = reader.build(env=env, prefix=(name.upper() + "_" + prefix))
+        else:
+            try:
+                val = reader(name, prefix, env)
+            except ValueError as e:
+                raise ValueError(
+                    f"Error parsing '{name}' property of '{cls.__inner_type.__name__}':\n"
+                    + str(e)
+                )
+
+        if val is not None:
+            args[name] = val
+
+    for k in cls.__fields.keys():
+        if k not in args:
+            raise KeyError()
 
     return cls.__inner_type(**args)
 
-class TemplateMeta(type):
 
+class TemplateMeta(type):
     def __new__(cls, typename, bases, ns):
-        if ns.get('_root', False):
+        if ns.get("_root", False):
             return super().__new__(cls, typename, bases, ns)
-        types = ns.get('__annotations__', {})
+        types = ns.get("__annotations__", {})
 
         inner_type = _make_nmtuple(typename, types.items())
 
-        ns['build'] = build
-        ns['__inner_type'] = inner_type
+        ns["build"] = build
+        ns["__inner_type"] = inner_type
 
         fields = {}
-        ns['__fields'] = dict([(n, _READERS.get(t) or t) for n, t in types.items()])
+        ns["__fields"] = dict([(n, _READERS.get(t) or t) for n, t in types.items()])
         result = type(typename, (object,), ns)
 
         return result
 
 
 class _TemplateMeta(type):
-
     def __new__(cls, typename, bases, ns):
-        types = ns.get('__annotations__', {})
+        types = ns.get("__annotations__", {})
         print(types)
         nm_tpl = _make_nmtuple(typename, types.items())
         defaults = []
@@ -126,10 +150,13 @@ class _TemplateMeta(type):
                 defaults.append(default_value)
                 defaults_dict[field_name] = default_value
             elif defaults:
-                raise TypeError("Non-default namedtuple field {field_name} cannot "
-                                "follow default field(s) {default_names}"
-                                .format(field_name=field_name,
-                                        default_names=', '.join(defaults_dict.keys())))
+                raise TypeError(
+                    "Non-default namedtuple field {field_name} cannot "
+                    "follow default field(s) {default_names}".format(
+                        field_name=field_name,
+                        default_names=", ".join(defaults_dict.keys()),
+                    )
+                )
         nm_tpl.__new__.__annotations__ = collections.OrderedDict(types)
         nm_tpl.__new__.__defaults__ = tuple(defaults)
         nm_tpl._field_defaults = defaults_dict
@@ -143,7 +170,8 @@ class _TemplateMeta(type):
 
         return nm_tpl
 
-class NamedTuple():
+
+class NamedTuple:
     """Typed version of namedtuple.
     Usage in Python versions >= 3.6::
         class Employee(NamedTuple):
@@ -161,16 +189,20 @@ class NamedTuple():
     In Python versions <= 3.5 use::
         Employee = NamedTuple('Employee', [('name', str), ('id', int)])
     """
+
     _root = True
 
     def __new__(self, typename, fields=None, **kwargs):
         if fields is None:
             fields = kwargs.items()
         elif kwargs:
-            raise TypeError("Either list of fields or keywords"
-                            " can be provided to NamedTuple, not both")
+            raise TypeError(
+                "Either list of fields or keywords"
+                " can be provided to NamedTuple, not both"
+            )
 
         return _make_nmtuple(typename, fields)
+
 
 class Template(metaclass=TemplateMeta):
     _root = True
--- src/tests/env_var_tests.py
+++ src/tests/env_var_tests.py
@@ -1,5 +1,5 @@
 import factorial
-from expects import expect, equal, be_false, be_a, be_true
+from expects import expect, equal, be_false, be_a, be_true, raise_error
 
 
 class simple_template(factorial.Template):
@@ -13,33 +13,36 @@ class nested_template(factorial.Template):
     force: bool
     stuff: simple_template
 
-class When_building_scalars_from_the_environment:
 
+class When_building_scalars_from_the_environment:
     def because_we_load_the_template(self):
-        self.cfg = simple_template.build(env={
-                'SIZE': '10',
-                'MY_BOOL': 'false',
-                'SOME_STR': 'sausage',
-                'BIGNESS': '0.2'
-            })
+        self.cfg = simple_template.build(
+            env={
+                "SIZE": "10",
+                "MY_BOOL": "false",
+                "SOME_STR": "sausage",
+                "BIGNESS": "0.2",
+            }
+        )
 
     def it_should_have_the_correct_values(self):
         expect(self.cfg.size).to(equal(10))
         expect(self.cfg.my_bool).to(be_false)
-        expect(self.cfg.some_str).to(equal('sausage'))
+        expect(self.cfg.some_str).to(equal("sausage"))
         expect(self.cfg.bigness).to(equal(0.2))
 
 
 class When_there_is_an_error_parsing_some_value:
-
     def because_we_load_the_template(self):
         try:
-            simple_template.build(env={
-                'SIZE': 'sausage',
-                'MY_BOOL': 'false',
-                'SOME_STR': 'sausage',
-                'BIGNESS': '0.2'
-            })
+            simple_template.build(
+                env={
+                    "SIZE": "sausage",
+                    "MY_BOOL": "false",
+                    "SOME_STR": "sausage",
+                    "BIGNESS": "0.2",
+                }
+            )
         except Exception as e:
             self.exn = e
 
@@ -47,37 +50,41 @@ class When_there_is_an_error_parsing_some_value:
         expect(self.exn).to(be_a(ValueError))
 
     def it_should_have_a_useful_message(self):
-        expect(str(self.exn)).to(equal(
-            "Error parsing 'size' property of 'simple_template':\n"
-            "invalid literal for int() with base 10: 'sausage'"))
+        expect(str(self.exn)).to(
+            equal(
+                "Error parsing 'size' property of 'simple_template':\n"
+                "invalid literal for int() with base 10: 'sausage'"
+            )
+        )
+
 
 class When_building_with_a_prefix:
-
     def because_we_load_the_template(self):
-        self.cfg = simple_template.build(env={
-                'PREFIX_SIZE': '10',
-                'PREFIX_MY_BOOL': 'false',
-                'PREFIX_SOME_STR': 'sausage',
-                'PREFIX_BIGNESS': '0.2'
+        self.cfg = simple_template.build(
+            env={
+                "PREFIX_SIZE": "10",
+                "PREFIX_MY_BOOL": "false",
+                "PREFIX_SOME_STR": "sausage",
+                "PREFIX_BIGNESS": "0.2",
             },
-            prefix="PREFIX")
+            prefix="PREFIX",
+        )
 
     def it_should_have_the_correct_values(self):
         expect(self.cfg.size).to(equal(10))
         expect(self.cfg.my_bool).to(be_false)
-        expect(self.cfg.some_str).to(equal('sausage'))
+        expect(self.cfg.some_str).to(equal("sausage"))
         expect(self.cfg.bigness).to(equal(0.2))
 
 
 class When_building_nested_configuration_objects:
-
     def given_an_environment(self):
         self.env = {
-            'FORCE': '1',
-            'STUFF_SIZE': '7',
-            'STUFF_MY_BOOL': '0',
-            'STUFF_SOME_STR': 'carbonara',
-            'STUFF_BIGNESS': '7.8'
+            "FORCE": "1",
+            "STUFF_SIZE": "7",
+            "STUFF_MY_BOOL": "0",
+            "STUFF_SOME_STR": "carbonara",
+            "STUFF_BIGNESS": "7.8",
         }
 
     def because_we_load_the_template(self):
@@ -87,6 +94,13 @@ class When_building_nested_configuration_objects:
         expect(self.cfg.force).to(be_true)
         expect(self.cfg.stuff.size).to(equal(7))
         expect(self.cfg.stuff.my_bool).to(be_false)
-        expect(self.cfg.stuff.some_str).to(equal('carbonara'))
+        expect(self.cfg.stuff.some_str).to(equal("carbonara"))
         expect(self.cfg.stuff.bigness).to(equal(7.8))
 
+
+class When_keys_are_missing:
+    def given_an_environment(self):
+        self.env = {"SIZE": "7", "MY_BOOL": "0", "SOME_STR": "carbonara"}
+
+    def it_should_raise_key_error(self):
+        expect(lambda: simple_template.build(env=self.env)).to(raise_error(KeyError))
