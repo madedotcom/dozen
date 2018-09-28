cc25068c0b9706705146550c38e2aae1ea500ace
Bob Gregory <bob@made.com>
2018-09-28 16:00:53 +0100

    Rename factorial to dozen, push 0.0.1

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(
-    name="py-12factorial",
+    name="dozen",
     version="0.0.1",
     description="Build type-safe config from env vars and consul.",
     python_requires=">=3.6.5",
@@ -11,5 +11,5 @@ setup(
     author="Bob Gregory",
     author_email="bob@made.com",
     url="https://github.com/madedotcom/py-factorial",
-    packages=["factorial", "tests"],
+    packages=["dozen", "tests"],
 )
